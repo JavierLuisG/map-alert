bd225c71f6db335c6713276eaae431be4f0ad9a2
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-09-26 17:11:31 -0500

    feat: add axios dependency; refactor AddAlert component styles for improved layout and semantics

--- src/app/(main)/add-alert/page.jsx
+++ src/app/(main)/add-alert/page.jsx
@@ -7,9 +7,9 @@ import Form from "../../components/form-add-alert/Form";
 const AddAlert = () => {
   return (
     <div className={styles.report_container}>
-      <div className={styles.title}>
-        <h2 className={styles.subtitle}>Reportar nueva alerta</h2>
-        <p>
+      <div className={styles.title_container}>
+        <h2 className={styles.title}>Reportar nueva alerta</h2>
+        <p className={styles.subtitle}>
           Ingresa los datos solicitados para reportar una situación y alertar a la comunidad
         </p>
       </div>
